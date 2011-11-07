d858192033181ef38d87788611f197253186c3d9
Keisuke Fukuda <fukuda@gmail.com>
2011-11-07 17:23:19 +0900

    temporary commit

---
+++ gen_readme.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os,os.path,re,sys
+from subprocess import Popen, PIPE
+
+if len(sys.argv) != 3:
+    sys.stderr.write("Usage: gen_readme.py INPUT OUTPUT")
+
+if sys.argv[1] == "-":
+    ifs = sys.stdin
+else:
+    ifs = open(sys.argv[1], 'r')
+
+if sys.argv[2] == "-":
+    ofs = sys.stdout
+else:
+    ofs = open(sys.argv[2], 'w')
+
+while True:
+    ln = ifs.readline()
+
+    if len(ln) == 0: break
+
+    ln = ln.strip()
+    m = re.match('^<%(.*)%>$', ln)
+    if m:
+        cmdline = m.group(1)
+        lines = Popen(['sh', '-c', cmdline], stdout=PIPE).communicate()
+        ofs.write(lines)
+    else:
+        ofs.write(ln + "\n")
+
+ifs.close()
+ofs.close()
+
+
